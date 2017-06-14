b5fbd984a15a06dee4fb31241a33f18d6a3e6e53
iPhantomiKX <cmpsun98@gmail.com>
2017-06-15 00:13:54 +0300

    Basic Player Movements
    -Amirul

--- MafiaTower/Assets/Scripts/PlayerController.cs
+++ MafiaTower/Assets/Scripts/PlayerController.cs
@@ -4,6 +4,11 @@ using UnityEngine;
 
 public class PlayerController : MonoBehaviour {
 
+    public Rigidbody2D rb;
+    public float speed;
+
+    private Vector2 velocity;
+
 	// Use this for initialization
 	void Start () {
 		
@@ -11,6 +16,14 @@ public class PlayerController : MonoBehaviour {
 	
 	// Update is called once per frame
 	void Update () {
-		
+        //Move player
+        Move();
 	}
+
+    void Move()
+    {
+        Vector2 MoveDirectionLR = new Vector2(Input.GetAxis("Horizontal"), Input.GetAxis("Vertical"));
+        velocity = MoveDirectionLR * speed;
+        rb.velocity = new Vector2(velocity.x, velocity.y);
+    }
 }
