1ec6effd118db7465ebc3574e980536ffee5ef3f
Noot Jettananurak <jc.nut92@gmail.com>
2017-06-16 11:42:48 +0300

    Enemy can walk between 2 objects

--- Assets/Scripts/EnemyController.cs
+++ Assets/Scripts/EnemyController.cs
@@ -5,10 +5,25 @@ using UnityEngine;
 public class EnemyController : MonoBehaviour {
 
 	public GameObject player;
+	public GameObject point1;
+	public GameObject point2;
+	GameObject target;
+
 	public float radius;
+
+	void Start(){
+		target = point1;
+	}
 
 	void Update () {
 		UpdateEnemyMovement ();
+	}
+
+	void OnCollisionEnter2D(Collision2D coll) {
+		if (coll.gameObject.Equals (point1))
+			target = point2;
+		else if (coll.gameObject.Equals (point2))
+			target = point1;
 	}
 
 	void OnDrawGizmosSelected()
@@ -20,11 +35,16 @@ public class EnemyController : MonoBehaviour {
 	void UpdateEnemyMovement()
 	{
 		float distance = Vector2.Distance (this.transform.position, player.transform.position);
-		if (distance < radius) 
-		{
+		if (distance < radius) {
 			Vector2 enemyPosition = this.transform.position;
 			Vector2 playerPosition = player.transform.position;
 			this.transform.position = Vector2.MoveTowards (this.transform.position, playerPosition, 1f * Time.deltaTime);
+		} else {
+			UpdateWalking ();
 		}
+	}
+
+	void UpdateWalking(){
+		this.transform.position = Vector2.MoveTowards (this.transform.position, target.transform.position, 0.7f * Time.deltaTime);
 	}
 }
