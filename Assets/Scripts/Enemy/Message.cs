86f588b3606192e961c374a75d4bc4ea0772a6c9
Angkunanuwat Prin TIX16 <prinangkunanuwat@kamk.fi>
2017-06-27 15:54:38 +0300

    Add enemy FSM assets

---
+++ Assets/Scripts/Enemy/Message.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Message  {
+
+	public enum MESSAGE_TYPE
+
+	{
+		ENEMY_SPOTPLAYER,   // Enemy Spot player and warn others
+		ENEMY_AREACLEAR		// Send Out that the area is clear
+	}
+
+	public MESSAGE_TYPE theMessageType;
+	public GameObject theSender, theReceiver, theTarget;
+	public Vector3 theDestination;
+}
---
+++ Assets/Scripts/Enemy/MessageBoard.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MessageBoard : MonoBehaviour {
+
+	List<Message> MessageList = new List<Message>();
+
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+	public void AddMessage(Message aMessage)
+	{
+		//Debug.Log("Adding a message.");
+		MessageList.Add (aMessage);
+	}
+
+
+
+	public Message GetMessage(int ID)
+	{
+		foreach (Message aMessage in MessageList)
+		{
+			if (aMessage.theReceiver.GetInstanceID() == ID)
+			{
+				Message temp = aMessage;
+				MessageList.Remove(aMessage);
+				//Debug.Log("Getting a message.");
+				return temp;
+			}
+		}
+		return null;
+	}
+}
