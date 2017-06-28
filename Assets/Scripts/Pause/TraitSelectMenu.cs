0cbe638147b1a9b8ca3344eb9bffe889e8984ecf
Sim Donovan TIX16 <donovandenzel@gmail.com>
2017-06-28 11:14:38 +0300

    Changed version

---
+++ Assets/Scripts/Pause/TraitSelectMenu.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+using UnityEngine.EventSystems;
+
+public class TraitSelectMenu : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+        if (Input.GetMouseButtonDown(1))
+        {
+            if (EventSystem.current.IsPointerOverGameObject())
+            {
+                Debug.Log("Click");
+            }
+        }
+
+	}
+
+    void OnMouseDown()
+    {
+        Debug.Log("Called");
+        Application.LoadLevel("SomeLevel");
+    }
+}
