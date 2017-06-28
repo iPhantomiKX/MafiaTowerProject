0ccdc04011b61a49179538259da814e31c97571e
Pitchaya Namchaisiri <pea14733@gmail.com>
2017-06-28 15:59:43 +0300

    Added Door in preparation to the Locked Door

---
+++ Assets/Scripts/Environment/Obstacles/Door.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Door : MonoBehaviour {
+    public bool locked;
+    public int keyID;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+    public void Open()
+    {
+        if (PlayerHasKey() || !locked) {
+            GetComponent<Collider2D>().enabled = false;
+        }
+
+    }
+
+    bool PlayerHasKey()
+    {
+        //Check if the player has the key or not
+        return false;
+    }
+}
