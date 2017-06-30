907784c004c85a78ab3b4e4b6f75500c491589c9
Jidapar Jettananurak <jc.nut92@gmail.com>
2017-06-30 11:53:20 +0300

    Add player animation

---
+++ Assets/Scripts/PlayerAnimationController.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerAnimationController : MonoBehaviour {
+
+	private Animator animator;
+
+	void Awake(){
+		animator = this.GetComponent<Animator> ();
+	}
+
+	void Start () {
+	}
+
+	void Update () {
+		if (PlayerController.shootButton && this.GetComponentInChildren<Gun>().ammo > 0) {
+			animator.Play ("PlayerShooting");
+		}
+
+	}
+}
