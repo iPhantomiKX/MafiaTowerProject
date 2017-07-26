cf953db7e78f3038a77343345de694689a8d9fe4
Sim Donovan TIX16 <donovandenzel@gmail.com>
2017-07-26 16:17:49 +0300

    Updated for of war

--- Assets/Scripts/Level Generation/FogManager.cs
+++ Assets/Scripts/Level Generation/FogManager.cs
@@ -44,7 +44,129 @@ public class FogManager : MonoBehaviour {
     void DoFogChecks()
     {
         // Get Player GridPos
+        Vector2 playerGridPos = levelManagerRef.GetGridPos(playerRef.transform.position);
 
-        //
+        // Recursive and switch off the relevant fogtiles
+        SwitchOffNearFog(playerGridPos, fogRange);
     }
+
+    public void SwitchOffNearFog(Vector2 startPos, int range)
+    {
+        List<Vector3> checkedList = new List<Vector3>();
+
+        RecursiveSwitchTileOff(startPos, range, checkedList);
+    }
+
+    void RecursiveSwitchTileOff(Vector2 checkPos, int tilesLeft, List<Vector3> checkedPos)
+    {
+        int x = (int)checkPos.x;
+        int y = (int)checkPos.y;
+
+        fogMap[x][y].GetComponent<FogTile>().SwitchOff();
+        checkedPos.Add(checkPos);
+
+        if (tilesLeft > 0)
+        {
+            Vector2 newPos = checkPos - new Vector2(0, 1);
+            if (!checkedPos.Contains(newPos))
+            {
+                RecursiveSwitchTileOff(newPos, tilesLeft - 1, checkedPos);
+            }
+
+            newPos = checkPos - new Vector2(0, -1);
+            if (!checkedPos.Contains(newPos))
+            {
+                RecursiveSwitchTileOff(newPos, tilesLeft - 1, checkedPos);
+            }
+
+            newPos = checkPos - new Vector2(1, 0);
+            if (!checkedPos.Contains(newPos))
+            {
+                RecursiveSwitchTileOff(newPos, tilesLeft - 1, checkedPos);
+            }
+
+            newPos = checkPos - new Vector2(-1, 0);
+            if (!checkedPos.Contains(newPos))
+            {
+                RecursiveSwitchTileOff(newPos, tilesLeft - 1, checkedPos);
+            }
+        }
+    }
+
 }
+
+/*
+    public List<BaseCharacter> GetCharactersInRange(Vector3 checkPos, int range)
+    {
+        List<BaseCharacter> returnList = new List<BaseCharacter>();
+        List<Vector3> checkedList = new List<Vector3>();
+
+        returnList = RecursiveFindCharacter(checkPos, range, checkedList);
+
+        return returnList;
+    }
+
+    List<BaseCharacter> RecursiveFindCharacter(Vector3 checkPos, int tilesLeft, List<Vector3> checkedPos)
+    {
+        List<BaseCharacter> returnList = new List<BaseCharacter>();
+
+        if (GetCharacterInTile(checkPos) != null)
+        {
+            returnList.Add(GetCharacterInTile(checkPos));
+            checkedPos.Add(checkPos);
+        }
+
+        if (tilesLeft > 0)
+        {
+            Vector3 newPos = checkPos - new Vector3(0, 1, 0);
+            List<BaseCharacter> tempList1 = new List<BaseCharacter>();
+            if (!checkedPos.Contains(newPos))
+            {
+                tempList1 = RecursiveFindCharacter(newPos, tilesLeft - 1, checkedPos);
+            }
+
+            newPos = checkPos - new Vector3(0, -1, 0);
+            List<BaseCharacter> tempList2 = new List<BaseCharacter>();
+            if (!checkedPos.Contains(newPos))
+            {
+                tempList2 = RecursiveFindCharacter(newPos, tilesLeft - 1, checkedPos);
+            }
+
+            newPos = checkPos - new Vector3(1, 0, 0);
+            List<BaseCharacter> tempList3 = new List<BaseCharacter>();
+            if (!checkedPos.Contains(newPos))
+            {
+                tempList3 = RecursiveFindCharacter(newPos, tilesLeft - 1, checkedPos);
+            }
+
+            newPos = checkPos - new Vector3(-1, 0, 0);
+            List<BaseCharacter> tempList4 = new List<BaseCharacter>();
+            if (!checkedPos.Contains(newPos))
+            {
+                tempList4 = RecursiveFindCharacter(newPos, tilesLeft - 1, checkedPos);
+            }
+
+            foreach (BaseCharacter aCharacter in tempList1)
+            {
+                returnList.Add(aCharacter);
+            }
+
+            foreach (BaseCharacter aCharacter in tempList2)
+            {
+                returnList.Add(aCharacter);
+            }
+
+            foreach (BaseCharacter aCharacter in tempList3)
+            {
+                returnList.Add(aCharacter);
+            }
+
+            foreach (BaseCharacter aCharacter in tempList4)
+            {
+                returnList.Add(aCharacter);
+            }
+        }
+
+        return returnList;
+    }
+ */
---
+++ Assets/Scripts/Level Generation/FogTile.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FogTile : MonoBehaviour {
+
+    public bool isOff = false;
+    public float timeToReappear = 1.0f;
+
+    float timer = 0.0f;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+        if (isOff)
+        {
+            timer -= Time.deltaTime;
+            if (timer > timeToReappear)
+            {
+                GetComponent<SpriteRenderer>().enabled = true;
+                timer = 0;
+            }
+        }
+	}
+
+    public void SwitchOff()
+    {
+        GetComponent<SpriteRenderer>().enabled = false;
+        isOff = true;
+    }
+}
